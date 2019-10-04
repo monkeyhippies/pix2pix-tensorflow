b8853b05f8e3b3e185d4e0472accad78abf92f27
root <root@ip-172-31-1-24.us-west-2.compute.internal>
2019-10-04 00:52:15 +0000

    Create package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='pix2pix',
+    version='0.1',
+    description='fork of pix2pix',
+    author='i dont know',
+    author_email='i dont know',
+    url='https://github.com/affinelayer/pix2pix-tensorflow',
+    packages=find_packages(),
+)
